5a18da84373862fafad5c3d7ee7a5d8a31b287ed
BhadriNarayana <bhadrinarayana445@gmail.com>
2023-05-16 13:09:53 +0530

    creating urls.py in projects for routing project related urls

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
+from django.shortcuts import HttpResponse
 # Create your views here.
+
+def projetcs(request):
+    return HttpResponse("Projects Main page")
+
+
+def hpage(request):
+    return HttpResponse("Home Page")
