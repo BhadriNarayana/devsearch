f71784fc8558b1b5eb0a95dd5366e45309921504
BhadriNarayana <bhadrinarayana445@gmail.com>
2023-05-16 14:25:35 +0530

    Created a main.html file for template inheritance and created included nav-bar in it, extended this on other templates

--- projects/views.py
+++ projects/views.py
@@ -3,11 +3,9 @@ from django.shortcuts import render
 from django.shortcuts import HttpResponse
 # Create your views here.
 
-def sprojects(request):
+def sprojects(request, pk):
         
         return render(request, 'single-project.html')
 
-def projects(request, pk):
-        if pk == None:
-                return render(request, 'projects.html')
-        return render(request, 'single-project.html')
+def projects(request):
+        return render(request, 'projects.html')
