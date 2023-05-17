ade6e1b857e3cdf7b73efe5f1be179311498542b
BhadriNarayana <bhadrinarayana445@gmail.com>
2023-05-17 21:50:48 +0530

    routed the form page with the urls.py

--- projects/urls.py
+++ projects/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('<str:pk>/', views.project, name = 'project'),
-    path('', views.projects, name = 'projects')
+    path('', views.projects, name = 'projects'),
+    path('create-project', views.create_project, name = 'create-project')
 ]
