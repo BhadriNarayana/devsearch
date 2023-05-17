fafe6d5d39736585dfc129a5b90b97aec7c122b4
BhadriNarayana <bhadrinarayana445@gmail.com>
2023-05-17 11:27:48 +0530

    ..

--- projects/models.py
+++ projects/models.py
@@ -9,7 +9,12 @@ class Project(models.Model):
     source_link = models.CharField(max_length=2000, null = True, blank = True)
 
     created = models.DateTimeField(auto_now_add=True)
-    
+    tags = models.ManyToManyField('Tag', blank = True)
+
+
+    votes_total = models.IntegerField(default=0, null = True, blank = True)
+    votes_ratio = models.IntegerField(default=0, null = True, blank = True)
+
     id = models.UUIDField(default=uuid.uuid4, unique=True,primary_key=True, editable=False)
 
     def __str__(self):
@@ -37,6 +42,14 @@ class Review(models.Model):
         return self.value
 
 
+class Tag(models.Model):
+    name = models.CharField(max_length=200)
+    id = models.UUIDField(default=uuid.uuid4, unique = True, primary_key=True, editable=True)
+
+    created = models.DateTimeField(auto_now_add = True)
+
+    def __str__(self):
+        return self.name
 
 
         
