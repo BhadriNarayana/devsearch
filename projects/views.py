640bea245681ed84a9b2914c93dc48a10f80c083
BhadriNarayana <bhadrinarayana445@gmail.com>
2023-05-17 21:57:33 +0530

    completed building skel of project  form

--- projects/views.py
+++ projects/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from .models import Project, Review, Tag
 from django.shortcuts import HttpResponse
+
+from .forms import ProjectForm
 # Create your views here.
 
 def project(request, pk):
@@ -11,3 +13,8 @@ def projects(request):
         projects = Project.objects.all()
         return render(request, 'projects/projects.html', {'projects':projects})
 
+def create_project(request):
+
+        form = ProjectForm()
+
+        return render(request, 'projects/project-form.html', {'form':form})
