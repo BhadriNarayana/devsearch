ff51de4249850ef31c059a328adfeb6ffcf6d8c1
BhadriNarayana <bhadrinarayana445@gmail.com>
2023-05-20 17:45:32 +0530

    testing

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1(fojf_owclv3s#z^u)dsb1^86otcalpa6)ks1l(lc_=r^jhml'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
