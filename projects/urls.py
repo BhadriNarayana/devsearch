8b0c8748b247149c8a3b9d960f826c290a17b06a
BhadriNarayana <bhadrinarayana445@gmail.com>
2023-05-20 07:37:32 +0530

    fix 3

--- projects/urls.py
+++ projects/urls.py
@@ -2,8 +2,6 @@
 from django.urls import path
 from . import views
 
-from django.conf import settings
-from django.conf.urls.static import static
 
 urlpatterns = [
     path('<str:pk>/', views.project, name = 'project'),
