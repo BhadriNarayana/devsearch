21f81a2d5474a958640fad7a9561883a5af1558f
BhadriNarayana <bhadrinarayana445@gmail.com>
2023-05-20 10:31:24 +0530

    staticfiles collection

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -24,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1(fojf_owclv3s#z^u)dsb1^86otcalpa6)ks1l(lc_=r^jhml'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+    
 
 # Application definition
 
