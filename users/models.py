f7a2c612d118e30252ad10e964a667bd1619411d
BhadriNarayana <bhadrinarayana445@gmail.com>
2023-06-02 20:29:26 +0530

    Added profile image field

--- users/models.py
+++ users/models.py
@@ -8,4 +8,6 @@ class Profile(models.Model):
     email = models.EmailField(max_length = 500, blank = True, null = True)
     short_intro = models.CharField(max_length = 200, blank = True, null = True)
     bio = models.TextField(blank = True, null = True)
-    
+    profile_image = models.ImageField(null = True, blank = True, upload_to = 'profiles/', default='profiles/default.png')
+    social_github = models.CharField(max_length = 200, blank=True, null = True)
+    social_github = models.CharField(max_length = 200, blank=True, null = True)
