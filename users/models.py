84af9e48cf7179680a2b193e686006c8a87efb62
BhadriNarayana <bhadrinarayana445@gmail.com>
2023-06-02 20:34:10 +0530

    Added created feild

--- users/models.py
+++ users/models.py
@@ -17,4 +17,8 @@ class Profile(models.Model):
 
     social_website = models.CharField(max_length = 200, blank=True, null = True)
 
+    created = models.DateTimeField(auto_now_add=True)
+
+    """ id = models. """
+
 
