c789d5659c336b97f2a5b7e695e8c2af585e09d3
BhadriNarayana <bhadrinarayana445@gmail.com>
2023-06-03 03:27:54 +0530

    Creating skills table4

--- users/models.py
+++ users/models.py
@@ -35,4 +35,4 @@ class Skill(models.Model):
     name = models.CharField(max_length=200, blank= True, null = True)
     description = models.TextField(null = True, blank = True)
     created = models.DateTimeField(auto_now_add=True)
-    
+    id = models.UUIDField(default=uuid.uuid4, unique=True,primary_key=True, editable=False)
