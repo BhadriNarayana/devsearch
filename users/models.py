e95b5cd8cd4a4438089b0d6fb73cd9578dc82850
BhadriNarayana <bhadrinarayana445@gmail.com>
2023-06-03 03:26:32 +0530

    Creating skills table2

--- users/models.py
+++ users/models.py
@@ -32,4 +32,5 @@ class Profile(models.Model):
 
 class Skill(models.Model):
     owner = models.ForeignKey(Profile, on_delete=models.CASCADE, null = True, blank = True)
+    name = models.CharField(max_length=200, blank= True, null = True)
     
