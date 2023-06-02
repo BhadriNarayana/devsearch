eef3a0dde466aa4ad7d7fd30a4f1aadc0dce2683
BhadriNarayana <bhadrinarayana445@gmail.com>
2023-06-02 19:52:41 +0530

    Added the email fieled

--- users/models.py
+++ users/models.py
@@ -5,4 +5,4 @@ from django.contrib
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete = models.CASCADE, null = True, blank = True)
     name = models.CharField(max_length = 200, blank = True, null = True)
-    email = models.EmailField(max_length = )
+    email = models.EmailField(max_length = 500, blank = True, null = True)
