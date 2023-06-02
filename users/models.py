529f0bb3522cbda377134bcbfac8b9487046a68d
BhadriNarayana <bhadrinarayana445@gmail.com>
2023-06-02 19:51:56 +0530

    Constructing the profile of the developers

--- users/models.py
+++ users/models.py
@@ -1,6 +1,8 @@
 from django.db import models
-
+from django.contrib
 # Create your models here.
 
 class Profile(models.Model):
-    
+    user = models.OneToOneField(User, on_delete = models.CASCADE, null = True, blank = True)
+    name = models.CharField(max_length = 200, blank = True, null = True)
+    email = models.EmailField(max_length = )
