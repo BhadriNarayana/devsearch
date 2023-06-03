38ab2716fd4ba1c95f387e2abc1473fee946dcd3
BhadriNarayana <bhadrinarayana445@gmail.com>
2023-06-03 16:56:35 +0530

    skills updation

--- users/views.py
+++ users/views.py
@@ -11,6 +11,9 @@ def profiles(request):
 
 def userProfile(request, pk):
     profile = Profile.objects.get(id = pk)
+
+    topSkills = Profile.skill_set.exclude(description__exact = "")
+    otherSkills
     return render(request, 'users/user-profile.html', {'profile':profile})
     
 
