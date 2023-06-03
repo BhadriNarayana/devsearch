fb6447d8fe8c9157e004675e3e62757274b76d64
BhadriNarayana <bhadrinarayana445@gmail.com>
2023-06-03 11:27:33 +0530

    user profile updation

--- users/views.py
+++ users/views.py
@@ -7,3 +7,10 @@ from .models import Profile
 def profiles(request):
     profiles = Profile.objects.all()
     return render(request, 'users/profiles.html', {'profiles':profiles})
+
+
+def userProfile(request, pk):
+    profile = Profile.objects.get(id = pk)
+    return render(request, 'users/user-profile.html', {'profile':profile})
+    
+
