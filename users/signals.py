7907d233a53cfd7f69a50f3bf0d967f4c8674b0f
BhadriNarayana <bhadrinarayana445@gmail.com>
2023-06-03 23:42:06 +0530

    signals logic for users app's db

--- users/signals.py
+++ users/signals.py
@@ -1,1 +1,9 @@
 from django.db.models.signals import post_save
+from .models import Profile
+
+
+def profileUpdated(sender, instance, created, **kwargs):
+    print("profile saved")
+
+
+post_save.connect(profileUpdated, sender = Profile)
