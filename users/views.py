9c313d42409dbc0fe47ddc41e986a5fa7c046397
BhadriNarayana <bhadrinarayana445@gmail.com>
2023-06-05 23:46:27 +0530

    User registration form feature

--- users/views.py
+++ users/views.py
@@ -58,7 +58,7 @@ def registerUser(request):
     form = CustomCreationForm()
 
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = CustomCreationForm(request.POST)
 
         if form.is_valid():
             user = form.save(commit=False)
