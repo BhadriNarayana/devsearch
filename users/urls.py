1e27c0f9fea0bdf7f908776e3761f777e722d7af
BhadriNarayana <bhadrinarayana445@gmail.com>
2023-06-05 16:05:16 +0530

    User login page

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from . import views
 urlpatterns = [
-    path('login/', views.loginPage, name = "login"),
+    path('login/', views.loginUser, name = "login"),
     path('', views.profiles, name = 'profiles'),
     path('profile/<str:pk>/', views.userProfile, name = 'profile'),
 
