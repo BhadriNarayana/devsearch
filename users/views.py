ac83f0133ef4dc0c5a23b4cc1edbd632c2f16d8b
BhadriNarayana <bhadrinarayana445@gmail.com>
2023-06-05 23:36:26 +0530

    User registration form feature

--- users/views.py
+++ users/views.py
@@ -73,5 +73,4 @@ def registerUser(request):
         else:
             messages.success(request, 'Error occured during registration')
 
-
     return render(request, 'users/login_register.html', {'page':page, 'form':form})
