355bc1a85fd82ee79654244bcd3f7afe5c2383ec
BhadriNarayana <bhadrinarayana445@gmail.com>
2023-06-05 23:43:56 +0530

    User registration form feature

--- users/forms.py
+++ users/forms.py
@@ -2,4 +2,7 @@ from django.forms import ModelForm
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
-class CustomCreationForm(UserCreationForm)
+class CustomCreationForm(UserCreationForm):
+    class Meta:
+        model = User
+        
