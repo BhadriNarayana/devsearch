bee40001b32973fd421ff31bbbbae50ac146fb40
BhadriNarayana <bhadrinarayana445@gmail.com>
2023-06-06 15:33:02 +0530

    Working on  edit account functionality

--- users/views.py
+++ users/views.py
@@ -3,7 +3,7 @@ from .models import Profile
 from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.models import User
 from django.contrib import messages
-
+from django.contrib.auth.decorators import login_required
 from .forms import CustomCreationForm
 # Create your views here.
 
@@ -75,6 +75,6 @@ def registerUser(request):
 
     return render(request, 'users/login_register.html', {'page':page, 'form':form})
 
-
+@login_required()
 def editAccount(request):
     return render(request, 'users/profile_form.html')
