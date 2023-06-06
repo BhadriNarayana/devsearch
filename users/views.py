5db6c0d1b1d2ccd3ebaa6f19839202402dc92087
BhadriNarayana <bhadrinarayana445@gmail.com>
2023-06-06 16:01:13 +0530

    Working on  edit account functionality

--- users/views.py
+++ users/views.py
@@ -5,6 +5,8 @@ from django.contrib.auth.models import User
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from .forms import CustomCreationForm
+
+from .forms import ProfileForm
 # Create your views here.
 
 def profiles(request):
