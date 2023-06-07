f5daaaff5f8191760e49ec3e2abc4095c9e78645
BhadriNarayana <bhadrinarayana445@gmail.com>
2023-06-07 17:25:03 +0530

    Editing user account template - crud functionality

--- users/forms.py
+++ users/forms.py
@@ -23,5 +23,5 @@ class CustomCreationForm(UserCreationForm):
 class ProfileForm(ModelForm):
     class Meta:
         model = Profile 
-        fields = ['name']
+        fields = ['name', 'email']
         
