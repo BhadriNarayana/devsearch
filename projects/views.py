21471b7a4ad7d2db02f5b58228f13eb0102c97bd
BhadriNarayana <bhadrinarayana445@gmail.com>
2023-06-08 02:43:16 +0530

    Edit functionality of projects and skills in the account template

--- projects/views.py
+++ projects/views.py
@@ -52,7 +52,7 @@ def update_project(request, pk):
 @login_required(login_url = "login")
 def delete_project(request, pk):
         profile = request.user.profile
-        project = Project.objects.get(id = pk)
+        project = profile.project_set.get(id = pk)
 
         if request.method == 'POST':
                 project.delete()
