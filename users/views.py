884c143ce5d90cd2250dca3ec7eb6951ab0d8196
BhadriNarayana <bhadrinarayana445@gmail.com>
2023-06-07 18:08:59 +0530

    Editing user account template - crud functionality

--- users/views.py
+++ users/views.py
@@ -97,4 +97,8 @@ def editAccount(request):
         if form.is_valid():
             form.save()
             return redirect('account')
+        else:
+            messages.error(request, "Error occured")
     return render(request, 'users/profile_form.html', {'form':form})
+
+
