9355ce072f0fbb519a29d967c0dcf5da2e0d8a74
BhadriNarayana <bhadrinarayana445@gmail.com>
2023-06-08 23:44:14 +0530

    working on search bar of the project page

--- projects/views.py
+++ projects/views.py
@@ -15,7 +15,6 @@ def projects(request):
         if request.GET.get('search_query'):
 
                 tags = Tag.objects.filter(name__icontains = search_query)
-
                 search_query = request.GET.get('search_query')
 
         projects = Project.objects.distinct().filter(Q(title__icontains = search_query) |
