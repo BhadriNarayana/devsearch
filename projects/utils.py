23b6099311e9006d2f4418ec12fd95c513f750ff
BhadriNarayana <bhadrinarayana445@gmail.com>
2023-06-09 00:01:11 +0530

    Editing util.py for projects app

--- projects/utils.py
+++ projects/utils.py
@@ -4,9 +4,8 @@ def searchProjects(request):
 
     search_query = ''
     if request.GET.get('search_query'):
-
-            tags = Tag.objects.filter(name__icontains = search_query)
-            search_query = request.GET.get('search_query')
+        tags = Tag.objects.filter(name__icontains = search_query)
+        search_query = request.GET.get('search_query')
 
     projects = Project.objects.distinct().filter(Q(title__icontains = search_query) |
                                         Q(description__icontains = search_query) |
