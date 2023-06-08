7df19ad2478753dff46ab2a457d53bb1b7454177
BhadriNarayana <bhadrinarayana445@gmail.com>
2023-06-08 09:41:18 +0530

    Editing functionality of editing skills

--- users/forms.py
+++ users/forms.py
@@ -1,7 +1,7 @@
 from django.forms import ModelForm
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from .models import Profile
+from .models import Profile, Skill
 
 class CustomCreationForm(UserCreationForm):
     class Meta:
