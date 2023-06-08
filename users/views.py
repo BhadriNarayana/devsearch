4be779334565421ec7e24ef30e559fc6ddb89cda
BhadriNarayana <bhadrinarayana445@gmail.com>
2023-06-08 11:14:34 +0530

    Editing functionality of editing skills

--- users/views.py
+++ users/views.py
@@ -130,7 +130,6 @@ def createSkill(request):
 def updateSkill(request, pk):
     profile = request.user.profile
     skill = profile.skill_set.get(id = pk)
-
     form = SkillForm(instance = skill)
     if request.method == 'POST':
         form = SkillForm(request.POST, instance = skill)
