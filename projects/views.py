6917f43d165d97e7399afd0693064fd7dfd210c6
BhadriNarayana <bhadrinarayana445@gmail.com>
2023-06-08 12:25:13 +0530

    Working on delete-skill functionality

--- projects/views.py
+++ projects/views.py
@@ -59,4 +59,4 @@ def delete_project(request, pk):
                 project.delete()
                 return redirect('projects')
 
-        return render(request, 'delete-object.html', {'project':project, 'page':page})
+        return render(request, 'delete-object.html', {'project':project, 'page': page})
