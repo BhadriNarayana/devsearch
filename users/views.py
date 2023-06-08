ca6ae55e7a476058e361b4399c27cb6868cfc24f
BhadriNarayana <bhadrinarayana445@gmail.com>
2023-06-08 15:18:22 +0530

    working on search bar of the profile page

--- users/views.py
+++ users/views.py
@@ -13,7 +13,9 @@ def profiles(request):
     search_query = ''
 
     if request.GET.get('search_query'):
-        search_query = request.GET.get('search_query')    
+        search_query = request.GET.get('search_query')
+
+    profiles = Profile.objects.filter(name__icontains = search_query)        
 
     profiles = Profile.objects.all()
     return render(request, 'users/profiles.html', {'profiles':profiles})
