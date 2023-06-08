18bd02b08d38df6a6c29aaa16147360a988fcb3b
BhadriNarayana <bhadrinarayana445@gmail.com>
2023-06-08 23:14:21 +0530

    working on search bar of the profile page

--- users/views.py
+++ users/views.py
@@ -7,12 +7,12 @@ from django.contrib.auth.decorators import login_required
 from .forms import CustomCreationForm, SkillForm
 
 from django.db.models import Q
-from .utils import searchProjects
+from .utils import searchProfiles
 from .forms import ProfileForm
 # Create your views here.
 
 def profiles(request):
-    profiles, search_query = searchProjects(request)
+    profiles, search_query = searchProfiles(request)
     return render(request, 'users/profiles.html', {'profiles':profiles, 'search_query':search_query})
 
 
