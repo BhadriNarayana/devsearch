ef68003cb07228dab56672452addf57a1fa5aa69
BhadriNarayana <bhadrinarayana445@gmail.com>
2023-06-08 22:55:07 +0530

    working on search bar of the profile page

--- users/views.py
+++ users/views.py
@@ -19,7 +19,7 @@ def profiles(request):
 
 
     skills = Skill.objects.filter(name__iexact = search_query)
-    profiles = Profile.objects.filter(Q(name__icontains = search_query) |
+    profiles = Profile.objects.distinct().filter(Q(name__icontains = search_query) |
                                        Q(short_intro__icontains = search_query) |
                                        Q(skills__in = skills)
                                        )   
