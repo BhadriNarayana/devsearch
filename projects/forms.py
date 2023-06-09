fd2b60912b5ecb572064bddf2ff2d9c7ca44393f
BhadriNarayana <bhadrinarayana445@gmail.com>
2023-06-10 00:25:55 +0530

    created a model form to enable posting of reviews

--- projects/forms.py
+++ projects/forms.py
@@ -29,3 +29,10 @@ class ReviewForm(ModelForm):
             'value': 'Place your vote',
             'body': 'Add comment with your vote'
         }
+
+    def __init__(self, *args, **kwargs):
+        super(ReviewForm, self).__init__(*args, **kwargs)
+
+        for name, field in self.fields.items():
+            field.widget.attrs.update({'class': 'input'})
+    
