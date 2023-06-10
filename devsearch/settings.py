3405664d462fbdaa56e799e12e971ae482a2d791
BhadriNarayana <bhadrinarayana445@gmail.com>
2023-06-10 17:10:53 +0530

    working on welcome mails functionality

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -127,7 +127,7 @@ EMAIL_PORT = 587
 
 EMAIL_USE_TLS = True
 
-EMAIL_HOST_USER = 'bhadrisdummy9@gmail.com'
+EMAIL_HOST_USER = 'bhadrinarayana445@gmail.com'
 
 EMAIL_HOST_PASSWORD = 'Bhadrisdummy789'
 
