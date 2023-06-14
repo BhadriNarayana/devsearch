aa04ab23d616d905052cf41b4d58f8330350b547
BhadriNarayana <bhadrinarayana445@gmail.com>
2023-06-14 14:39:19 +0530

    Added the email automation part with google app passwords

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -150,7 +150,7 @@ USE_TZ = True
 
 
 
-""" 
+
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 EMAIL_HOST = 'smtp.gmail.com'
@@ -159,10 +159,10 @@ EMAIL_PORT = 587
 
 EMAIL_USE_TLS = True
 
-EMAIL_HOST_USER = 'bhadrinarayana445@gmail.com'
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 
-EMAIL_HOST_PASSWORD = 'jvvfgzazcmifsjst'
- """
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+
 
 
 
